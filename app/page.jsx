72b0e4bccdefbab6221ecd04fc6316bd24ab48a2
fufex3934 <fufawakgari174@gmail.com>
2025-04-25 14:39:08 +0300

    first module

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
+'use client'
 import { useState } from 'react';
  
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
  
-function HomePage() {
+export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
  
   const [likes, setLikes] = useState(0);
